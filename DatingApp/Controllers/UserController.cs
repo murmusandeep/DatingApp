0024d01ec24070b414f476a8cdea720063efa583
Sandeep Murmu <sandeep.m@edifybrokers.com>
2023-01-16 12:55:38 +0530

    first commit

---
+++ DatingApp/Controllers/UserController.cs
@@ -0,0 +1,35 @@
+﻿using DatingApp.Data;
+using DatingApp.Entities;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace DatingApp.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UserController : ControllerBase
+    {
+        private readonly DataContext _dataContext;
+
+        public UserController(DataContext dataContext)
+        {
+            _dataContext = dataContext;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> GetUsers() 
+        { 
+            var users = await _dataContext.Users.ToListAsync();
+
+            return Ok(users);
+        }
+
+        [HttpGet("{id}")]
+        public async Task<IActionResult> GetUser(int Id)
+        {
+            var user = await _dataContext.Users.FindAsync(Id);
+            return Ok(user);
+        }
+    }
+}
